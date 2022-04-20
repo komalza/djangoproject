67165cfa15c7b26f6d537eab7147339ffc841f17
komalza <zahrakomal87@gmail.com>
2022-04-20 17:00:51 +0500

    initial commit

---
+++ mysite/home/view.py
@@ -0,0 +1,28 @@
+# i have created this file _komal
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from .models import Contact
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact1 = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact1.save()
+    return render(request, 'contact.html')
+
+
+def services(request):
+    return render(request, 'services.html')
